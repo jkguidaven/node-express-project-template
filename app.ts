75d6ddfecd2a9b0f326dc33b7714d0eb9b89d32f
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-03-17 14:44:20 +0800

    add compression middleware

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as http from 'http';
 import * as bodyparser from 'body-parser';
+import compression from 'compression';
 
 import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
@@ -16,11 +17,14 @@ const debugLog: debug.IDebugger = debug('app');
 // We load our static files from source directory
 app.use(config.STATIC_BASE_URL, express.static(config.STATIC_SOURCE_DIR));
 
-// here we are adding middleware to parse all incoming requests as JSON
+// We are adding middleware to parse all incoming requests as JSON
 app.use(bodyparser.json());
 
-// here we are adding middleware to allow cross-origin requests
-app.use(cors());
+// We are adding middleware to allow cross-origin requests
+app.use(cors(config.corsOptions));
+
+// We are adding middleware to enable compression on server responses
+app.use(compression());
 
 // here we are configuring the expressWinston logging middleware,
 // which will automatically log all HTTP requests handled by Express.js
--- config/app.config.ts
+++ config/app.config.ts
@@ -14,5 +14,17 @@ export default {
      * Specify here the base url for serving the static asset files.
      * By default it is set to '/public'
      */
-    STATIC_BASE_URL: '/public'
+    STATIC_BASE_URL: '/public',
+
+    /*
+     * Specify here the CORS configuration. refer to the following
+     * documentation to learn more about the options:
+     * https://www.npmjs.com/package/cors#configuring-cors
+    */
+    corsOptions: {
+        origin: "*",
+        methods: ['GET', 'HEAD', 'PUT', 'PATH', 'POST', 'DELETE'],
+        preflightContinue: false,
+        optionsSuccessStatus: 204
+    }
 };
