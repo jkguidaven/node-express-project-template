41f69d29796aec426d54580d66b2d730ba42789b
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-03-19 17:04:15 +0800

    Use webpack to bundle the app

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-
-// We will need to extract our static files to the dist folder so
-// the compiled app will be able to locate the files
-gulp.task('copy-assets', function() {
-  return gulp.src('./src/static/**/*')
-    .pipe(gulp.dest('./dist/src/static'))
-  ;
-});
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const { NODE_ENV = 'production' } = process.env;
+
+module.exports = {
+    entry: './app.ts',
+    mode: NODE_ENV,
+    watch: NODE_ENV === 'development',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['ts-loader']
+            }
+        ]
+    },
+
+    externals: [
+        // We will need this so we won't bundle unnecessary codes from node_modules
+        nodeExternals()
+    ],
+    plugins: [
+        // Copy our asset folder to build directory
+        new CopyPlugin({
+            patterns: [{ from: 'src/static', to: 'static' }]
+        })
+    ]
+};
