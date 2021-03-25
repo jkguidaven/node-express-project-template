1b4cabc70ee5c5b985d40ed7f9a2106f72bc7fa9
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-03-25 18:03:06 +0800

    remove debug log when app is initialize

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import prodConfig from './config/prod.config';
 import devConfig from './config/dev.config';
-import debugLog from './src/utils/debug';
 
 const config: AppConfig =
     process.env.environment === 'production' ? prodConfig : devConfig;
@@ -11,8 +10,6 @@ import AppConfig from './config/app.config';
 // Initialize our app using our config file
 const app = initialize(config);
 
-const server = app.listen(config.PORT, () => {
-    debugLog(`Server running at http://localhost:${config.PORT}`);
-});
+const server = app.listen(config.PORT);
 
 export default server;
